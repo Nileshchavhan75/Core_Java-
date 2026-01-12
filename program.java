01dd1713022cd4f3e3aa937553754b6f66c4442a
Nilesh Chavhan <nchavhan544@gmail.com>
2026-01-12 17:43:17 +0530

    new program

---
+++ program.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class program {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the Size of an Array: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for(int i = 0; i<arr.length; i++){
+            System.out.print("Enter "+i+" Element: ");
+            arr[i] = sc.nextInt();
+        }
+
+        int start = 0;
+        int end = arr.length-1;
+        while (start<end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+
+            start++;
+            end--;
+        }
+        for(int x : arr){
+            System.out.print(x+ " ");
+        }
+        sc.close();
+    }
+}
+
+    
