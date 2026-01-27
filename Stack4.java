e25fd1329fedfa6c1dd7d106e64695d563f98e02
Nilesh Chavhan <nchavhan544@gmail.com>
2026-01-27 23:09:32 +0530

    new program

--- Stack4.java
+++ Stack4.java
@@ -3,7 +3,7 @@ public class Stack4 {
     public static void main(String[] args){
         Stack<Integer> st = new Stack<>();
         st.push(10);
-        st.push(20);
+        st.push(22);
         st.push(30);
         st.push(40);
         st.push(55);
