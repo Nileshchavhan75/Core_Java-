378a6623a3b3701b32dc72181de397ee2243ac35
Nilesh Chavhan <nchavhan544@gmail.com>
2026-02-05 18:53:26 +0530

    new program

---
+++ arr.java
@@ -0,0 +1,10 @@
+public class arr {
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+        arr[0] = 1;
+        arr[1] = 2;
+
+        System.out.println(arr);
+
+    }
+}
