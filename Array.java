f4fffe6c6cd39a9ee83a93a38d54ae6e9d01828f
Nilesh Chavhan <nchavhan544@gmail.com>
2026-02-05 18:49:57 +0530

    new program

--- Array.java
+++ Array.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class Array {
     public static void main(String args[]){
-        int[] array = {1,2,3,4,8};
+        int[] array = {1,2,3,5,8};
         int x = array.length;
         System.out.println("number: " + array[2]);
         for(int i = 1; i <= x; i++){
