b795f37d8ae260944da92ee8dfe37d1591496e8b
Nilesh Chavhan <nchavhan544@gmail.com>
2026-02-09 17:50:20 +0530

    new program

---
+++ Solution22.java
@@ -0,0 +1,8 @@
+class Solution22 {
+    public int findLUSlength(String a, String b) {
+        if (a.equals(b)) {
+            return -1;
+        }
+        return Math.max(a.length(), b.length());
+    }
+}
