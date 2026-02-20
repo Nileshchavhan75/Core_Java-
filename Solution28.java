c8980e94ec50697c206c9c70a06040dfe37a9b32
Nilesh Chavhan <nchavhan544@gmail.com>
2026-02-20 19:11:27 +0530

    new program

---
+++ Solution28.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution28 {
+    public int findLongestChain(int[][] pairs) {
+        
+        // Step 1: Sort by second value
+        Arrays.sort(pairs, (a, b) -> a[1] - b[1]);
+        
+        int count = 1;
+        int lastEnd = pairs[0][1];
+        
+        for(int i = 1; i < pairs.length; i++){
+            if(pairs[i][0] > lastEnd){
+                count++;
+                lastEnd = pairs[i][1];
+            }
+        }
+        
+        return count;
+    }
+}
