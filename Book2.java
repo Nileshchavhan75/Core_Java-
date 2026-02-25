e26b266a8a10a2f7f3a051e6ebbbeed09890e91d
Nilesh Chavhan <nchavhan544@gmail.com>
2026-02-25 20:25:52 +0530

    new program

---
+++ Book2.java
@@ -0,0 +1,23 @@
+class Book2{
+    String title;
+    String author;
+    double price;
+
+
+    void displayDetails(){
+        System.out.println("TItle: "+ title);
+        System.out.println("Author: "+ author);
+        System.out.println("Price: "+price);
+    }
+}
+public class opps1 {
+    public static void main(String[] args) {
+        Book b = new Book();
+        b.title = "rich dad poor dad";
+        b.author = "RObert";
+        b.price = 157.2;
+
+        b.displayDetails();
+    }
+    
+}
