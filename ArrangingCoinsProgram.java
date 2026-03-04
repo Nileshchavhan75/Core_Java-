3e47e7ae1b59d58ad64d5f78e2254bb292d9ecb1
Nilesh Chavhan <nchavhan544@gmail.com>
2026-03-04 18:05:11 +0530

    new program

---
+++ ArrangingCoinsProgram.java
@@ -0,0 +1,31 @@
+public class ArrangingCoinsProgram {
+
+    public static void main(String[] args) {
+        int n = 8;
+        System.out.println(arrangeCoins(n));
+    }
+
+    public static int arrangeCoins(int n) {
+        
+        long left = 0;
+        long right = n;
+        
+        while(left <= right){
+            
+            long mid = left + (right - left) / 2;
+            long coins = mid * (mid + 1) / 2;
+            
+            if(coins == n){
+                return (int) mid;
+            }
+            else if(coins < n){
+                left = mid + 1;
+            }
+            else{
+                right = mid - 1;
+            }
+        }
+        
+        return (int) right;  // largest valid k
+    }
+}
