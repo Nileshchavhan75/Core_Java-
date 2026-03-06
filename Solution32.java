1564c5fcd8a682386973e0e3fd4acf3901a5f73b
Nilesh Chavhan <nchavhan544@gmail.com>
2026-03-06 20:56:53 +0530

    new program

---
+++ Solution32.java
@@ -0,0 +1,13 @@
+class Solution32 {
+    public boolean checkOnesSegment(String s) {
+         for(int i = 1; i < s.length(); i++){
+
+            if(s.charAt(i) == '1' && s.charAt(i-1) == '0'){
+                return false;
+            }
+
+        }
+
+        return true;
+    }
+}
